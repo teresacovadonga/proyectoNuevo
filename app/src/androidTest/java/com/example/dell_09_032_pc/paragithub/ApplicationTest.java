09fa04b9c5ad9fc72dcfe897170a777a3a2d5777
teresacovadonga <teresacovadonga49@gmail.com>
2016-06-17 14:06:26 +0200

    Primer commit

---
+++ app/src/androidTest/java/com/example/dell_09_032_pc/paragithub/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.dell_09_032_pc.paragithub;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/dell_09_032_pc/paragithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell_09_032_pc.paragithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
